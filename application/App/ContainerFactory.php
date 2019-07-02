57eea597427afb53107cb4346570ef7985226710
Adrian Suter <adrian@suter-wirz.ch>
2019-07-02 11:02:02 +0200

    Add php-di container factory

---
+++ application/App/ContainerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use DI\ContainerBuilder;
+use Exception;
+use Psr\Container\ContainerInterface;
+
+class ContainerFactory
+{
+    /**
+     * @param string $rootPath
+     *
+     * @return ContainerInterface
+     * @throws Exception
+     */
+    public static function create(string $rootPath): ContainerInterface
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->addDefinitions([
+            Preferences::class => new Preferences($rootPath),
+        ]);
+        $containerBuilder->addDefinitions($rootPath . '/application/config/container-definitions.php');
+
+        // Note: In production, you should enable container-compilation.
+        // $containerBuilder->enableCompilation($rootPath . '/cache');
+
+        return $containerBuilder->build();
+    }
+}
---
+++ application/config/container-definitions.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Preferences;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+return [
+    LoggerInterface::class => function (ContainerInterface $container) {
+        // Get the preferences from the container.
+        $preferences = $container->get(Preferences::class);
+
+        // Instantiate a new logger and push a handler into the logger.
+        $logger = new Logger('slim4-skeleton');
+        $logger->pushHandler(
+            new RotatingFileHandler(
+                $preferences->getRootPath() . '/logs/slim4-skeleton.log'
+            )
+        );
+
+        return $logger;
+    },
+];
